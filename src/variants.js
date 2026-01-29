2894940547a3d7df758094ed10c190cbf2c90a95
Archis <vazearchis@gmail.com>
2026-01-29 11:07:12 +0530

    added variants

--- src/variants.js
+++ src/variants.js
@@ -17,6 +17,15 @@ export const VARIANTS = {
         borderWidth: 3,
     },
 
+    dark: {
+        surfaceColor: '#181818',
+        sideColor: '#494949',
+        textColor: '#e5e7eb',
+        bordered: true,
+        borderColor: '#5e5e5e',
+        borderWidth: 3,
+    },
+
     arcade: {
         surfaceColor: '#22b8c7',
         sideColor: '#e5dd4a',
@@ -39,9 +48,9 @@ export const VARIANTS = {
         surfaceColor: '#2a2a37',
         sideColor: '#8b5e3c',
         textColor: '#c4a484',
-        bordered: false,
-        borderColor: 'transparent',
-        borderWidth: 0,
+        bordered: true,
+        borderColor: '#8b5f39',
+        borderWidth: 4,
     },
 
     warning: {
@@ -50,6 +59,33 @@ export const VARIANTS = {
         textColor: '#fff7ed',
         bordered: true,
         borderColor: '#d9445b',
+        borderWidth: 4,
+    },
+
+    steel: {
+        surfaceColor: '#cbd5e1',
+        sideColor: '#64748b',
+        textColor: '#020617',
+        bordered: true,
+        borderColor: '#475569',
+        borderWidth: 2,
+    },
+
+    gold: {
+        surfaceColor: '#ffcf10',
+        sideColor: '#d29000',
+        textColor: '#422006',
+        bordered: true,
+        borderColor: '#fff0a8',
+        borderWidth: 2,
+    },
+
+    lavender: {
+        surfaceColor: '#cec3fd',
+        sideColor: '#b489ff',
+        textColor: '#2e1065',
+        bordered: false,
+        borderColor: '#a36eff',
         borderWidth: 4,
     },
 };
